4da64ffbacfcba44e1f787abec3998d7eef4f44f
Aishwarya <Aishwarya@DESKTOP-JRBKBKJ>
2017-08-02 13:50:32 -0400

    Brute Force solution for finding unique triplets that add to a given sum.

--- src/week3/UniqueTriplets.java
+++ src/week3/UniqueTriplets.java
@@ -1,9 +1,44 @@
 package week3;
 
+import java.util.Arrays;
+//program to find unique triplets adding to a given number 
+//taking into consideration an unsorted array
 public class UniqueTriplets {
 
 	public static void main(String[] args) {
-		
+		int array[]={-1,0,2,1,-1,1,5};
+		int sum=0;
+		int result[][]=tripletArrayBruteForce(array,sum);
+		for(int i=0;i<result.length;i++){
+			for(int j=0;j<result[i].length;j++){
+					System.out.print(result[i][j]);
+			}
+			System.out.println();
+		}
 	}
-
+	private static int[][] tripletArrayBruteForce(int[] array,int sum){
+		int returnArray[][]=new int[array.length][3];
+		int n=0;
+		for(int i=0;i<array.length;i++){
+			for(int j=1;j<array.length;j++){
+				for(int k=2;k<array.length;k++){
+					if((array[i]+array[j]+array[k])==sum){
+						int subArray[]={array[i],array[j],array[k]};
+						Arrays.sort(subArray);	
+						if(uniqueCheck(returnArray,subArray)){
+								returnArray[n++]=subArray;
+						}
+					}
+				}
+			}
+		}
+		return returnArray;
+	}
+	private static boolean uniqueCheck(int returnArray[][],int subArray[]){
+		for(int i=0;i<returnArray.length;i++){
+			if(Arrays.equals(returnArray[i], subArray))
+				return false;
+		}
+		return true;
+	}
 }
