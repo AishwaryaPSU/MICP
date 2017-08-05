82b5e3ebf970803e319604d190b6c14e3dc06170
Aishwarya <Aishwarya@DESKTOP-JRBKBKJ>
2017-08-05 00:08:14 -0400

    Optimal solution

--- src/week3/UniqueTriplets.java
+++ src/week3/UniqueTriplets.java
@@ -9,15 +9,27 @@ public class UniqueTriplets {
 		int array[]={-1,0,2,1,-1,1,5};
 		int sum=0;
 		int result[][]=tripletArrayBruteForce(array,sum);
+		System.out.print("[");
 		for(int i=0;i<result.length;i++){
+			System.out.print("[");
 			for(int j=0;j<result[i].length;j++){
+				if(j==result[i].length-1){	
 					System.out.print(result[i][j]);
+				}
+				else System.out.print(result[i][j]+",");
 			}
-			System.out.println();
+			if(i==result.length)
+				System.out.print("]");
+			else
+				System.out.print("],");
+
 		}
+		System.out.print("]");
 	}
 	private static int[][] tripletArrayBruteForce(int[] array,int sum){
 		int returnArray[][]=new int[array.length][3];
+		if(array.length<3)
+			return null;
 		int n=0;
 		for(int i=0;i<array.length;i++){
 			for(int j=1;j<array.length;j++){
